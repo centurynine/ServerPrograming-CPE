f41796280634febe9bd80108e48393c6abd8007a
CenturyNine <lowlifeix@gmail.com>
2023-01-21 02:27:20 +0700

    Basic api

--- NodeJS-1/app.js
+++ NodeJS-1/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+let port = '80'
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "NodeJS-1/pages");
@@ -8,14 +9,24 @@ app.use(express.json())
 app.get('', (req, res)=> {
   res.render('index')
 })
+app.get('/', (req, res)=> {
+  res.render('index')
+})
 app.get('/home', (req, res)=> {
   res.render('index')
 })
 app.get('/register', (req, res)=> {
   res.render('register')
 })
-app.listen(3000, function () {
-  console.log("Listening on port 3000");
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
 });
+app.post('/addUser', async (req, res) => {
+  let data = req.body
+  const datsun = await JSON.parse(fs.readFileSync('NodeJS-1/data.json'))
+  datsun.push(data)
+  fs.writeFileSync('data.json', JSON.stringify(datsun))
+  res.send('succeeds')
+})
 
 
